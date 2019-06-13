0c58ebf322e74752f7ed65e9e3c2d8fdb9ee0ee8
Rachel Salazar <ret1992@gmail.com>
2019-06-12 19:00:21 -0700

    add app-grocery component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppGroceryComponent
   ],
   imports: [
     BrowserModule,
